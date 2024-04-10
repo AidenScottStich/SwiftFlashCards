8665e927559eae570d96363402fa7f35485f53e0
Aiden <stichaiden2@gmail.com>
2024-04-10 10:00:44 -0500

    Initial Commit

---
+++ SwiftFlashCards/SwiftFlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFlashCardsApp.swift
+//  SwiftFlashCards
+//
+//  Created by STICH, AIDEN S. on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
